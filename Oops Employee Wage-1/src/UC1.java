e97766128c2499616c26e04733477521b247a3aa
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-03-22 18:07:18 +0530

    UC1

---
+++ Oops Employee Wage-1/src/UC1.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class UC1 {
+	static int random(){
+		Random ran = new Random();
+		int i = ran.nextInt(2)+1;
+		return i;
+	}
+
+	static String attendence(){
+		String att;
+		if (random()==1){
+			att = "Present";
+		}
+		else   {
+			att = "Absent";
+		}
+		return att;
+	}
+
+
+	public static void main(String[] args){
+		System.out.println("The Employee is "+attendence());
+	}
+}
+
